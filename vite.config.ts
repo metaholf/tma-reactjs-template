215c7f3507368c3ffa07e98a202814ebd3911da6
Alex K <exp1773air@gmail.com>
2026-05-14 02:54:19 +0300

    chore: update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template/',
+  base: '/tma-reactjs-template/',
   css: {
     preprocessorOptions: {
       scss: {
